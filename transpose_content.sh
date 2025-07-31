14c564d62a1fe926993cb2b09ac79d11a170750b
Mamiololo01 <jaycees20@yahoo.com>
2025-07-31 15:45:15 +0100

    added new files

---
+++ transpose_content.sh
@@ -0,0 +1,35 @@
+# Given a text file file.txt, transpose its content.
+# 
+# You may assume that each row has the same number of columns and each field is separated by the ' ' character.
+# 
+# For example, if file.txt has the following content:
+# 
+# name age
+# alice 21
+# ryan 30
+# 
+# Output the following:
+# 
+# name alice ryan
+# age 21 30
+##################################################################################
+
+#!/bin/sh
+
+# Read from the file file.txt and print its transposed content to stdout.
+
+awk '
+{
+    for (i = 1; i <= NF; i++){
+        if (NR == 1){
+            s[i] = $1;
+        }else{
+            s[i] = s[i] " " $1;
+        }
+    }
+}
+END {
+    for (i = 1; s[i] != ""; i++){
+        print s[i];
+    }
+}' file.txt
